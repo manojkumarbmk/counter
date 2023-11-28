08e6a7fb60da0b223f2ba82605156f68ac5ad2f5
manojkumarbmk <147690551+manojkumarbmk@users.noreply.github.com>
2023-11-28 11:55:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const counter = document.querySelector('.counter');
+const incr = document.querySelector('.incr');
+const decr = document.querySelector('.decr');
+let count = 0;
+
+incr.addEventListener("click", () => {
+    count = count + 1;
+    counter.innerHTML = count;
+});
+decr.addEventListener("click", () => {
+    count = count - 1;
+    counter.innerHTML = count;
+});
+
